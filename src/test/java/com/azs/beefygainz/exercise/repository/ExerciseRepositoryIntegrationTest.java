a9725b06743fa60c0064717522a6bc619ebc07c9
Andrew Sutherland <azsuth@gmail.com>
2019-04-19 10:32:15 -0500

    jpa integration tests

---
+++ src/test/java/com/azs/beefygainz/exercise/repository/ExerciseRepositoryIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.azs.beefygainz.exercise.repository;
+
+import com.azs.beefygainz.exercise.model.Exercise;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ExerciseRepositoryIntegrationTest {
+
+    @Autowired
+    ExerciseRepository exerciseRepository;
+
+    @Before
+    public void setUp() {
+        exerciseRepository.save(Exercise.builder().name("Bench Press").notes("Notes 1").userId("asdf").build());
+        exerciseRepository.save(Exercise.builder().name("Lat Pulldowns").notes("Notes 2").userId("asdf").build());
+        exerciseRepository.save(Exercise.builder().name("Squats").notes("Notes 3").userId("fdsa").build());
+    }
+
+    @Test
+    public void findAllByUserId() {
+        Iterable<Exercise> exercises = exerciseRepository.findAllByUserId("asdf");
+
+        assertEquals(2, StreamSupport.stream(exercises.spliterator(), false).count());
+        exercises.forEach(exercise -> {
+            assertEquals("asdf", exercise.getUserId());
+        });
+    }
+
+    @Test
+    public void findByIdAndUserId() {
+        Optional<Exercise> exercise = exerciseRepository.findByIdAndUserId(3L, "fdsa");
+
+        assertTrue(exercise.isPresent());
+        assertEquals("fdsa", exercise.get().getUserId());
+        assertFalse(exerciseRepository.findByIdAndUserId(3L, "asdf").isPresent());
+        assertFalse(exerciseRepository.findByIdAndUserId(4L, "fdsa").isPresent());
+    }
+}
---
+++ src/test/java/com/azs/beefygainz/exercise/repository/SetRepositoryIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.azs.beefygainz.exercise.repository;
+
+import com.azs.beefygainz.exercise.model.Exercise;
+import com.azs.beefygainz.exercise.model.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class SetRepositoryIntegrationTest {
+
+    @Autowired
+    ExerciseRepository exerciseRepository;
+    @Autowired
+    SetRepository setRepository;
+
+    @Before
+    public void setUp() {
+        Exercise exercise1 = Exercise.builder().build();
+        exercise1.addSet(Set.builder().reps(12).lbs(125).exercise(exercise1).notes("Notes 1").build());
+        exercise1.addSet(Set.builder().reps(13).lbs(125).exercise(exercise1).notes("Notes 2").build());
+
+        Exercise exercise2 = Exercise.builder().build();
+        exercise2.addSet(Set.builder().reps(6).lbs(175).exercise(exercise2).notes("Notes2").build());
+
+        exerciseRepository.save(exercise1);
+        exerciseRepository.save(exercise2);
+    }
+
+    @Test
+    public void findByIdAndExercise() {
+        Optional<Set> set = setRepository.findByIdAndExercise(1L, Exercise.builder().id(1L).build());
+
+        assertTrue(set.isPresent());
+        assertEquals(Long.valueOf(1L), set.get().getExercise().getId());
+        assertFalse(setRepository.findByIdAndExercise(3L, Exercise.builder().id(1L).build()).isPresent());
+    }
+}
